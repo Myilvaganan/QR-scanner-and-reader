4c27b7840f533a844e5c80378ded188411f7131d
Myilvaganan [C] Sakthivel <myilvaga@amazon.com>
2022-08-29 19:08:25 +0530

    Updated Details Box

--- src/components/QRScanner.jsx
+++ src/components/QRScanner.jsx
@@ -28,33 +28,39 @@ const QRScanner = (props) => {
         </div>
         {!showCamera && (
           <div className="row bg-light bg-gradient p-4">
+            <h3>Tracking Details</h3>
             <div className="col-12  d-flex flex-column justify-content-start align-items-start">
-              <p>
-                <b>Container: {data?.container}</b>
-              </p>
-              <p>
-                <b>Vessel No:{data?.vessel}</b>
-              </p>
-              <p>
-                <b>Tube No:{data?.tube}</b>
-              </p>
-              <p>
-                <b>Location: {data?.location}</b>
-              </p>
-              <label for="floatingTextarea" className="mb-2">
-                <b>Comments:</b>
-              </label>
-              <div style={{ width: "50%" }}>
-                <textarea
-                  className="form-control mb-4"
-                  placeholder="Leave a comment here"
-                  id="floatingTextarea"
-                ></textarea>
-              </div>
+              <ul class="list-group w-100">
+                <li className="list-group-item d-flex justify-content-start">
+                  <b className="me-4">Container</b> {data?.container}
+                </li>
+                <li className="list-group-item d-flex justify-content-start">
+                  <b className="me-4">Vessel No</b>
+                  {data?.vessel}
+                </li>
+                <li className="list-group-item d-flex justify-content-start">
+                  <b className="me-4">Tube No</b>
+                  {data?.tube}
+                </li>
+                <li className="list-group-item d-flex justify-content-start">
+                  <b className="me-4">Location</b> {data?.location}
+                </li>
+                <li className="list-group-item d-flex justify-content-start">
+                  <label for="floatingTextarea" className="mb-2">
+                    <b className="me-4">Comments: </b>
+                  </label>
 
-              <div className="d-flex justify-content-center align-items-center">
+                  <textarea
+                    className="form-control mb-4"
+                    placeholder="Leave a comment here"
+                    id="floatingTextarea"
+                  ></textarea>
+                </li>
+              </ul>
+
+              <div className="d-flex justify-content-end align-items-end mt-3 w-100">
                 <button
-                  className="btn btn-outline-warning mx-2"
+                  className="btn btn-warning mx-2"
                   onClick={() =>
                     (window.location.href = "../QR-scanner-and-reader")
                   }
@@ -62,7 +68,7 @@ const QRScanner = (props) => {
                   Previous
                 </button>
                 <button
-                  className="btn btn-outline-success"
+                  className="btn btn-success"
                   onClick={() =>
                     (window.location.href = "../QR-scanner-and-reader")
                   }
