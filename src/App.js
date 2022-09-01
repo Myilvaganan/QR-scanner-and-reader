0c03a52dcfcaa4977c2fb7d2464bb60e1fde9254
Myilvaganan [C] Sakthivel <myilvaga@amazon.com>
2022-09-01 09:24:23 +0530

    Removed Title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
         <>
             <div className="container d-flex flex-column justify-content-center align-items-center mb-5">
                 <img className="mt-3" src={logo} alt="Logo" width="50px" height="50px" />
-                <h2 className="text-center my-2 text-dark"> Shipment Tracking App</h2>
                 <QR />
             </div>
             <Footer />
